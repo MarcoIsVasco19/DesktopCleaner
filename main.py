db65645fc3f0d0f58a3f7967383f26f1a402d606
Marco <dunlopmarco@gmail.com>
2018-09-03 15:46:04 +0200

    Added cross platform functionality

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os, shutil
+from sys import platform as _platform
+from os.path import expanduser
 
 ####################
 #### VARIABLES #####
 ####################
+
+home = expanduser("~")
 
 # Types of files
 pictures = ('.png', '.jpeg')
@@ -10,31 +14,42 @@ documents = ('.docx', '.pdf', '.txt')
 scripts = ('.sh', '.py',)
 media = ('.mp3', '.avi', '.mkv')
 
-# Username
-mac_username = 'marcomarques'
-
-# Directories needed
-search_dir = '/Users/' + mac_username + '/Desktop/'
-pictures_dir = '/Users/' + mac_username + '/Desktop/pictures/'
-documents_dir = '/Users/' + mac_username + '/Desktop/documents/'
-scripts_dir = '/Users/' + mac_username + '/Desktop/scripts/'
-media_dir = '/Users/' + mac_username + '/Desktop/media/'
-misc_dir = '/Users/' + mac_username + '/Desktop/misc/'
-
-ignore_folders = [pictures_dir, documents_dir, scripts_dir,
-                  media_dir, misc_dir]
-
+# Check which OS is running and
+# Create the required directory variables
+# linux or MAC OS X
+if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
+    if os.path.isdir(home+"/Desktop/"):
+        search_dir = home+"/Desktop/"
+        pictures_dir = search_dir+"pictures/"
+        documents_dir = search_dir+"documents/"
+        scripts_dir = search_dir+"scripts/"
+        media_dir = search_dir + "media/"
+        misc_dir = search_dir+"misc/"
+    else:
+        desktop = "Your " + _platform + " does not have a Desktop Directory"
+# Windows 32-bit or 64-bit
+elif _platform == "win32" or _platform == "win64":
+    if os.path.isdir(home+"\Desktop"):
+        search_dir = home+"\Desktop\\"
+        pictures_dir = search_dir+"\pictures\\"
+        documents_dir = search_dir+"\documents\\"
+        scripts_dir = search_dir+"\scripts\\"
+        media_dir = search_dir + "\media\\"
+        misc_dir = search_dir+"\misc\\"
+    else:
+        desktop = "Your " + _platform + " does not have a Desktop Directory"
 
 ####################
 #### FUNCTIONS #####
 ####################
+
 
 # Check if Directory exist, otherwise create it.
 def directoryCheck(pdir):
     if os.path.isdir(str(pdir)):
         return True
     else:
-        os.system('mkdir -v ' + str(pdir))
+        os.mkdir(str(pdir))
         return True
 
 
