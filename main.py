ea49a71a680c51bfc1edb76da8ddb44fc7d7b5b2
Marco <dunlopmarco@gmail.com>
2018-09-03 12:45:18 +0200

    Moving files works. Errors when sub directory already exists.

--- main.py
+++ main.py
@@ -15,11 +15,14 @@ mac_username = 'marcomarques'
 
 # Directories needed
 search_dir = '/Users/' + mac_username + '/Desktop/'
-pictures_dir = '/Users/' + mac_username + '/Desktop/pictures'
-documents_dir = '/Users/' + mac_username + '/Desktop/documents'
-scripts_dir = '/Users/' + mac_username + '/Desktop/scripts'
-media_dir = '/Users/' + mac_username + '/Desktop/media'
-misc_dir = '/Users/' + mac_username + '/Desktop/misc'
+pictures_dir = '/Users/' + mac_username + '/Desktop/pictures/'
+documents_dir = '/Users/' + mac_username + '/Desktop/documents/'
+scripts_dir = '/Users/' + mac_username + '/Desktop/scripts/'
+media_dir = '/Users/' + mac_username + '/Desktop/media/'
+misc_dir = '/Users/' + mac_username + '/Desktop/misc/'
+
+ignore_folders = [pictures_dir, documents_dir, scripts_dir,
+                  media_dir, misc_dir]
 
 
 ####################
@@ -35,12 +38,12 @@ def directoryCheck(pdir):
         return True
 
 
-def moveFile(folder, src, dst):
-    filetomove = (str(folder) + str(src))
-    movingfileto = (str(dst) + str(file))
-    shutil.move(filetomove, movingfileto)
+def moveFile(src, dst):
+    filetomove = (str(search_dir) + str(src))
+    shutil.move(filetomove, str(dst))
 
 
+# Checks the extension of the file to the Tuple at the start
 def searchInList(filetocheck, array):
     if filetocheck.endswith(array):
         return True
@@ -55,14 +58,24 @@ def searchInList(filetocheck, array):
 # Walk through the Desktop Directory
 for folderName, subfolders, filenames in os.walk(search_dir):
     print('The current folder is ' + folderName, '\n')
-
-    # Print all files inside the Desktop Directory
-    # print('FILES INSIDE ' + folderName + ': ')
-    # for filename in filenames:
-    #     print(filename)
     for file in filenames:
+        # Pictures
         if searchInList(file, pictures):
             directoryCheck(pictures_dir)
-            moveFile(search_dir, file, pictures_dir)
+            moveFile(file, pictures_dir)
+
+        # elif searchInList(file, documents_dir):
+        #     directoryCheck(documents_dir)
+        #     moveFile(file, documents_dir)
+        #
+        # elif searchInList(file, scripts_dir):
+        #     directoryCheck(scripts_dir)
+        #     moveFile(file, scripts_dir)
+        # elif searchInList(file, media_dir):
+        #     directoryCheck(media_dir)
+        #     moveFile(file, media_dir)
+        # else:
+        #     directoryCheck(misc_dir)
+        #     moveFile(file, misc_dir)
 
     print('')
